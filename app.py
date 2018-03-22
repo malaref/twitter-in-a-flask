04de52caf13049fa51f71a4ac0487dea0ea31c42
muhammad-alaref <muhammad.alaref96@eng-st.cu.edu.eg>
2018-03-22 21:14:55 +0200

    Implemented the user data scrambling functionality!

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import os
 
+import sys
+import twitter
+from flask import Flask, request, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.update(
@@ -11,13 +13,36 @@ app.config.update(
 db = SQLAlchemy(app)
 
 
-@app.route('/users/<twitterId>/', methods=['GET'])
-def user(twitter_id):
-    return "Hello {}!".format(twitter_id)
+import models
 
 
-@app.route('/users/<twitterId>/posts/', methods=['GET'])
-def posts(twitter_id):
+twitter_api = twitter.Api(
+    consumer_key=os.environ['CONSUMER_KEY'],
+    consumer_secret=os.environ['CONSUMER_SECRET'],
+    access_token_key=os.environ['ACCESS_TOKEN_KEY'],
+    access_token_secret=os.environ['ACCESS_TOKEN_SECRET'])
+
+
+@app.route('/users/<twitter_id>/', methods=['GET'])
+def user_data(twitter_id):
+    try:
+        user = models.User.query.get(twitter_id)
+        if not request.args.get('local'):
+            updated_user_dict = twitter_api.GetUser(user_id=twitter_id).AsDict()
+            if user:
+                user.update(updated_user_dict)
+            else:
+                user = models.User(updated_user_dict)
+                db.session.add(user)
+            db.session.commit()
+        return jsonify(dict(user))
+    except Exception as e:
+        print >> sys.stderr, e
+        abort(404)
+
+
+@app.route('/users/<twitter_id>/posts/', methods=['GET'])
+def user_posts(twitter_id):
     return "Posts of {}!".format(twitter_id)
 
 
--- models.py
+++ models.py
@@ -2,5 +2,26 @@ from app import db
 
 
 class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime)
+    url = db.Column(db.String())
+    screen_name = db.Column(db.String())
+    name = db.Column(db.String())
+    description = db.Column(db.String())
+    location = db.Column(db.String())
+    followers_count = db.Column(db.Integer)
+    friends_count = db.Column(db.Integer)
+    listed_count = db.Column(db.Integer)
+    statuses_count = db.Column(db.Integer)
 
-    pass
+    def update(self, user_dict):
+        for key in user_dict:
+            if hasattr(User, key):
+                setattr(self, key, user_dict[key])
+
+    def __init__(self, user_dict):
+        self.update(user_dict)
+
+    def __iter__(self):
+        for column in self.__table__.columns:
+            yield column.name, getattr(self, column.name)
